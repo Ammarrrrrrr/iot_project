f126d5b60053a1918c317d97f1dff31821918bb5
Ammar <surfmemo58@gmail.com>
2024-12-09 18:48:59 +0000

    psss

--- esp_dht22.ino/esp_dht22.ino.ino
+++ esp_dht22.ino/esp_dht22.ino.ino
@@ -8,20 +8,22 @@
 
 #define DHTPIN 5
 #define DHTTYPE DHT22
-//#define POTPIN A0
-//#define AirQuality A1  
+#define LED_PIN 
+
 DHT dht(DHTPIN, DHTTYPE);
 
 char auth[] = BLYNK_AUTH_TOKEN;
 
-// Replace with your Wi-Fi credentials
-const char* ssid = "ssid";
-const char* password = "password";
+const char* ssid = "WEHOME11";
+const char* password = "4080740807";
 
 void setup() {
   // Debug console
   Serial.begin(9600);
-  
+
+  // Set LED pin as output
+  pinMode(LED_PIN, OUTPUT);
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -33,6 +35,9 @@ void setup() {
 
   Blynk.begin(auth, ssid, password);
   Serial.println("Blynk started");
+
+  // Initialize the random number generator
+  randomSeed(analogRead(A0));
 }
 
 void loop() {
@@ -40,13 +45,16 @@ void loop() {
 
   // Read sensor values
   float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
- // float noiseLevel = analogRead(POTPIN);
-  //float airQuality = analogRead(AirQuality);
 
-  // Check if readings are valid
+
+  float humidity = dht.readHumidity();
+  // Generate random values for noise level and air quality
+  int noiseLevel = random(30, 100); // Simulated noise level (dB)
+  int airQuality = random(0, 500);  // Simulated air quality index (AQI)
+
   if (!isnan(temperature)) {
     Blynk.virtualWrite(V1, temperature); 
+    if (temperature > 24.00) {digitalWrite(LED_PIN, HIGH);} else {digitalWrite(LED_PIN,LOW );}
   } else {
     Serial.println("Failed to read temperature from DHT sensor");
   }
@@ -56,10 +64,10 @@ void loop() {
   } else {
     Serial.println("Failed to read humidity from DHT sensor");
   }
-  
-  //Blynk.virtualWrite(V3, noiseLevel); 
-  
-  //Blynk.virtualWrite(V4, airQuality);
+
+  // Send random values to Blynk
+  Blynk.virtualWrite(V3, noiseLevel); 
+  Blynk.virtualWrite(V4, airQuality);
 
   delay(2000);
 }
